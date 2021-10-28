a81c8a059abbfb1d383d7903b9b74ff0a27fea46
Rayhan B <64879192+rayb26@users.noreply.github.com>
2021-10-28 14:25:12 -0400

    added comments

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+"""
+Author: Rayhan Biju
+Version: October 28, 2021
+Description: File contains functionality to run the web app and contains necessary components to initialize
+a Flask app.
+"""
+
 from website import create_app
-from flask import render_template
 
 app = create_app()
-
+# Debug is set to false because this app will be pushed to production
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
